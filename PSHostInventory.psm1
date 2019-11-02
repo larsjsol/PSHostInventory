c3205ba5a7a4edb8fc140f4b40c237e640ee7c42
Lars Jørgen Solberg <supersolberg@gmail.com>
2019-11-02 13:35:52 +0100

    first pass on PS module

---
+++ PSHostInventory.psm1
@@ -0,0 +1,70 @@
+﻿Class InventoryItem
+{
+    [String]$FQDN
+    [IPAddress[]]$IpAddresses
+    [String]$TicketReference
+    [String]$Description
+    [Inventory]$Inventory
+
+    InventoryItem() {}
+    InventoryItem([String]$FQDN, [IPAddress[]]$IpAddresses, [String]$TicketReference, [String]$Description, [Inventory]$Inventory)
+    {
+        $this.FQDN, $this.IpAddresses, $this.TicketReference, $this.Description, $this.Inventory = $FQDN, $IpAddresses, $TicketReference, $Description, $Inventory
+    }
+}
+
+Class Inventory
+{
+    [String]$Name = "Unnamed"
+    [String]$TypeName = "Base Inventory"
+    hidden [InventoryItem[]]$Items = $()
+
+    Inventory() {}
+    Inventory([String]$Name)
+    {
+        $this.Name = $Name
+    }
+
+    Refresh()
+    {
+        # default implementation simply creates an empty list
+        $this.Items = @()
+    }
+
+    [InventoryItem[]] GetInventoryItems()
+    {
+        if ($this.Items.Count -eq 0)
+        {
+            $this.Refresh()
+        }
+        return $this.Items
+    }
+}
+
+Class DummyInventory : Inventory
+{
+    [String]$TypeName = "Dummy Inventory"
+
+    DummyInventory(){}
+    DummyInventory([string]$Name) : base ($Name) {}
+
+    Refresh()
+    {
+        $items = @()
+        $this.Items += [InventoryItem]::new("test.lol.no", "192.168.0.1", "TEST-1001", "Test av lol", $this)
+        $this.Items += [InventoryItem]::new("test.megalol.no", @("192.168.0.2", "10.0.02"), "TEST-2001", "Test av megalol 2000", $this)
+    }
+}
+
+class OpenDCIMInventory : Inventory
+{
+    [String]$TypeName = "openDCIM"
+    [Uri]$APIURI
+    [String]$APIKey
+
+    OpenDCIMInventory(){}
+    OpenDCIMInventory([string]$Name, [Uri]$APIURI, [String]$APIKey) : base ($Name)
+    {
+        $this.APIURI, $this.APIKey = $APIURI, $APIKey
+    }
+}
---
+++ Script.ps1
@@ -0,0 +1,17 @@
+﻿using module .\PSHostInventory.psm1
+
+$Inventories = @()
+$Inventories += [DummyInventory]::new("Test inventory")
+$Inventories += [OpenDCIMInventory]::new("DCIM", "http://example.com", "s3cr3t")
+
+$InventoryItems = @()
+$Summary = @{}
+foreach ($Inventory in $Inventories)
+{
+    $Items = $Inventory.GetInventoryItems()
+    $Summary.Add($Inventory.Name, $Items.Count)
+    $InventoryItems += $Items
+}
+
+$InventoryItems| format-table FQDN, IpAddresses, @{ Label="Inventory"; Expression={$_.Inventory.Name} }, TicketReference, Description
+$Summary | format-table @{ Label="Inventory"; Expression={$_.Name} }, @{ Label="Number of items"; Expression={$_.Value} }
